b01ae989731e4526cdb3ab8387c2f388c70f4d11
andySouthwick <asouthwick01@gmail.com>
2016-12-28 19:04:34 -0700

    event handlers with get requests

--- scripts/controllers/WeatherController.js
+++ scripts/controllers/WeatherController.js
@@ -1,6 +1,29 @@
 app.controller('weatherCtrl', function($scope, $http){
-  $http.get("https://simple-weather.p.mashape.com/weatherdata?lat=40.618691&lng=-112.004918&mashape-key=DA0Uuhi5U8mshkZmeLlaBH6h4ekQp1RxmkbjsnCzVLVed42yUg")
-  .then(function(response){
-      $scope.weather = response.data;
-  });
+
+     //$scope.localWeather = function(location){
+
+           
+
+                $scope.westJordan = function(){
+                $scope.location = "lat=40.618691&lng=-112.004918";
+                console.log($scope.location);
+             $http.get("https://simple-weather.p.mashape.com/weatherdata?"+ $scope.location + "&mashape-key=DA0Uuhi5U8mshkZmeLlaBH6h4ekQp1RxmkbjsnCzVLVed42yUg")
+    .then(function(response){
+        $scope.weather = response.data;
+  });}
+                $scope.chicago = function(){
+                    $scope.location = "lat=41.8336479&lng=-87.8722357";
+                console.log($scope.location);     
+            $http.get("https://simple-weather.p.mashape.com/weatherdata?"+ $scope.location + "&mashape-key=DA0Uuhi5U8mshkZmeLlaBH6h4ekQp1RxmkbjsnCzVLVed42yUg")
+    .then(function(response){
+        $scope.weather = response.data;
+  });}
+            
+           
+       
+    // }
+
+
 });
+
+//west jordan 
